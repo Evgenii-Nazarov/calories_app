1efe61bb39dea108620afab5be03db0c0e9b809a
Evgenii-Nazarov <ev66647260@gmail.com>
2021-01-14 01:19:12 -0800

    Updates

---
+++ precache-manifest.0d1448aa182f8be13c211eecd72a7817.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45d03e1e6228bdc40fd412b64ea213e",
+    "url": "/calories_app/index.html"
+  },
+  {
+    "revision": "74fb59eb0ff0e6c4e249",
+    "url": "/calories_app/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "679857b47ac5ce73542e",
+    "url": "/calories_app/static/css/main.5df883ac.chunk.css"
+  },
+  {
+    "revision": "74fb59eb0ff0e6c4e249",
+    "url": "/calories_app/static/js/2.845d2d32.chunk.js"
+  },
+  {
+    "revision": "6053e451d7862a9dc5b0526c27e6d0db",
+    "url": "/calories_app/static/js/2.845d2d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679857b47ac5ce73542e",
+    "url": "/calories_app/static/js/main.3c70ee32.chunk.js"
+  },
+  {
+    "revision": "9fc2195100e2b6b182af",
+    "url": "/calories_app/static/js/runtime-main.6c03dcb4.js"
+  }
+]);
